5ef49e7e7a1db4df3f092f766f78624ef4edc519
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2026-04-08 09:28:37 +0200

    INIT : creation environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  spotify: {
+    clientId: 'TON_CLIENT_ID_ICI',
+    redirectUri: 'https://ton-domaine.com/callback',
+    scopes: 'user-top-read user-read-private'
+  },
+  openai: {
+    apiKey: 'TA_CLE_OPENAI_ICI'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: 'TON_CLIENT_ID_ICI',
+    redirectUri: 'http://localhost:4200/callback',
+    scopes: 'user-top-read user-read-private'
+  },
+  openai: {
+    apiKey: 'TA_CLE_OPENAI_ICI'
+  }
+};
