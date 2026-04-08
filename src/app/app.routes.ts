c331c6cfdc8f4be6433c912dc2f707825b396ce3
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2026-04-08 11:56:43 +0200

    MAJOR : configuration routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Loading } from './pages/loading/loading';
+import { Error } from './pages/error/error';
 
 export const routes: Routes = [
-        
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'loading', component: Loading },
+  { path: 'error', component: Error }
 ];
